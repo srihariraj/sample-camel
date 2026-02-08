432639d74847516e13bd7b3bd2a5ffe9caa6dea4
srihariraj <srihariatom@gmail.com>
2026-02-08 22:33:39 +0530

    consumer

--- CamelJMSConsumer/src/main/java/com/tutorials/ktvideos/listener/AsciiArtConsumerRoute.java
+++ CamelJMSConsumer/src/main/java/com/tutorials/ktvideos/listener/AsciiArtConsumerRoute.java
@@ -11,8 +11,11 @@ public class AsciiArtConsumerRoute {
 
 	@Autowired
 	private AsciiArtService service;
+
 	@JmsListener(destination = "${spring.artemis.embedded.queues}")
 	public void receive(String message) {
+
 		service.validateAndSave(message);
-    }
+
+	}
 }
